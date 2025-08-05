9ba2dce8026eaf10146a5a586ee4ac7a1a9c75d2
aziz-shoko <azizbek.shokosimov@gmail.com>
2025-08-04 22:38:45 -0500

    added property based testing learning

---
+++ documents/testing/propertybasedtests/roman.go
@@ -0,0 +1,51 @@
+package propertybasedtests
+
+import (
+	"strings"
+)
+
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(num uint16) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for num >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			num -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
+
+func ConvertToArabic(roman string) uint16 {
+	arabic := uint16(0)
+
+	for _, sym := range allRomanNumerals {
+		for strings.HasPrefix(roman, sym.Symbol) {
+			arabic += sym.Value
+			roman = strings.TrimPrefix(roman, sym.Symbol)
+		}	
+	}
+	return arabic
+}
